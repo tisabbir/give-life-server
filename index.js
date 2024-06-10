ccdb754e1403f6431ebb734ad0104a185ae3f79c
TI Sabbir <tisabbir0@gmail.com>
2024-06-10 08:37:06 +0600

    donation status can be now updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bq6unn4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -88,6 +88,19 @@ async function run() {
         const result = await donationRequestCollection.insertOne(donationRequest);
         res.send(result);
 
+    })
+
+    app.patch('/donationRequests/:id', async(req,res)=>{
+        const id = req.params.id;
+        const updatedRequest = req.body;
+        const filter = {_id : new ObjectId(id)};
+        const updatedDoc = {
+            $set:{
+                donationStatus : updatedRequest.donationStatus,
+            }
+        }
+        const result = await donationRequestCollection.updateOne(filter,updatedDoc);
+        res.send(result);
     })
 
     // Send a ping to confirm a successful connection
